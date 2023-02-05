e21ab22111167be57a87f7af7ab1e105f51afe79
nacho0403 <natsuho.ogi@gmail.com>
2023-02-06 01:15:15 +0900

    7-2

---
+++ var/www/html/menu.php
@@ -0,0 +1,9 @@
+<a href="../../../../../Downloads/php/chapter7/product.php">商品</a>
+<a href="../../../../../Downloads/php/chapter7/favorite-show.php">お気に入り</a>
+<a href="../../../../../Downloads/php/chapter7/history.php">購入履歴</a>
+<a href="../../../../../Downloads/php/chapter7/cart-show.php">カート</a>
+<a href="../../../../../Downloads/php/chapter7/purchase-input.php">購入</a>
+<a href="login-input.php">ログイン</a>
+<a href="../../../../../Downloads/php/chapter7/logout-input.php">ログアウト</a>
+<a href="../../../../../Downloads/php/chapter7/customer-input.php">会員登録</a>
+<hr>
