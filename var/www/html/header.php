b2c9fa8eed57c62920b6ad1fc088ae355c213db3
nacho0403 <natsuho.ogi@gmail.com>
2023-02-11 02:33:43 +0900

    8-2

--- var/www/html/header.php
+++ var/www/html/header.php
@@ -4,6 +4,6 @@
 <meta charset="UTF-8">
 <title>PHP Sample Programs</title>
 <link rel="stylesheet" href="../style.css">
-<link rel="stylesheet" href="../../../../../Downloads/php/style.css">
+<link rel="stylesheet" href="../../../../../../Downloads/php/style.css">
 </head>
 <body>
