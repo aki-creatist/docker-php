5c9ea51c0af9deab26a13ae6a7292a310acaca9a
akifumi_nakanishi <akifumi.nakanishi@gmail.com>
2024-10-27 16:55:08 +0900

    ok

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CsvParamsKey;
+
 class MasterController extends Controller
 {
+    /**
+     * php cli.php --uri=master/create --some-option=1
+     * @param $options
+     * @return void
+     */
     public function create($options = [])
     {
+        $params = $this->paramsFactory->make(['1', '2', '3', 'あわ'], CsvParamsKey::class);
+        var_dump($params);
+        var_dump($options);die;
         var_dump($this->masterModel->all());
     }
 }
